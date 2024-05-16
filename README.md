a7da2ea640680b3e6f2c22f503e5154c07a996eb
edwin_torres <ea.torresl1@uniandes.edu.co>
2024-05-16 17:27:19 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## importar datos desde un API
---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import json
+
+# Leer el archivo JSON
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+# Crear un DataFrame a partir del JSON
+df = pd.DataFrame(data)
+
+# Imprimir el tamaño del DataFrame
+print(df.shape)
