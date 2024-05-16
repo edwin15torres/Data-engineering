7106581864e5ab7b94c67c9d48c72b7e13f7a89e
edwin_torres <ea.torresl1@uniandes.edu.co>
2024-05-16 17:37:43 -0500

    Migración a SQLite

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import json
+import sqlite3
 
 # Leer el archivo JSON
 with open('source/data.json', 'r') as file:
@@ -7,6 +8,15 @@ with open('source/data.json', 'r') as file:
 
 # Crear un DataFrame a partir del JSON
 df = pd.DataFrame(data)
+
+# Conectar a la base de datos SQLite (o crearla si no existe)
+conn = sqlite3.connect('database.db')
+
+# Exportar los datos del DataFrame a la tabla 'datos' en la base de datos SQLite
+df.to_sql('datos', conn, if_exists='replace', index=False)
+
+# Cerrar la conexión a la base de datos
+conn.close()
 
 # Imprimir el tamaño del DataFrame
 print(df.shape)
